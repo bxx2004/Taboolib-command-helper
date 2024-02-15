76077bed7d2cc148ee37ff35aec7c11dc147b6dc
bxx <102979712+bxx2004@users.noreply.github.com>
2024-02-15 15:38:43 +0800

    Update CommandHelper.kt

--- CommandHelper.kt
+++ CommandHelper.kt
@@ -1,4 +1,6 @@
+
 import taboolib.common.platform.ProxyCommandSender
+import taboolib.common.platform.command.CommandContext
 import taboolib.common.platform.command.CommandStructure
 import taboolib.common.platform.command.component.CommandComponent
 import taboolib.common.platform.command.component.CommandComponentDynamic
@@ -6,12 +8,48 @@ import taboolib.common.platform.command.component.CommandComponentLiteral
 import taboolib.module.chat.ComponentText
 import taboolib.module.chat.colored
 import taboolib.module.chat.component
+import kotlin.math.ceil
 
 /**
  * 多语言命令帮助模板
  * @author bxx2004
  * @since 2024/02/14 20:08
  */
+fun CommandContext<out Any>.error(msg: String,index:Int=-1,lang: String ="zh_cn"){
+    val errorIndex = if (index == -1) args().lastIndex else index
+    var arg = "&c/$name ${args().joinToString(" ").replace(args()[errorIndex],"&c&l&n"+args()[errorIndex])}&c"
+    var joinArgs = arg
+    if (joinArgs.length > 25){
+        if (errorIndex<=1){
+            joinArgs = arg.substring(0,25).replace(args()[errorIndex],"&c&l&n"+args()[errorIndex]+"&c")
+        }else{
+            joinArgs = "&c/$name ... &c&l&n${args()[errorIndex]}&c ..."
+        }
+    }
+    joinArgs += "  &7&o->${Language[lang,"error"]}\n"
+    val fixIndex = joinArgs.indexOf(args()[errorIndex]) + args()[errorIndex].length / 2
+    joinArgs += "&c${space(fixIndex-3)} └── "
+    val message = ArrayList<String>()
+    for (i in 0..ceil(msg.length / 10.00).toInt()) {
+        try {
+            message.add(try {
+                msg.substring(i*10,i*10+10)
+            }catch (e:Exception){
+                msg.substring(i*10,msg.length)
+            })
+        }catch (e:Exception){
+
+        }
+    }
+    joinArgs += message[0] +"\n"
+    for ((index1, s) in message.withIndex()) {
+        if (index1 != 0){
+            joinArgs += "&c${space(fixIndex-1)} $s"
+        }
+    }
+    sender().sendMessage(joinArgs.colored())
+}
+
 private object Language{
     operator fun get(lang:String,node:String):String{
         when(lang){
@@ -21,6 +59,7 @@ private object Language{
                     "alias"->"别名"
                     "permission"->"权限"
                     "parameter"->"参数"
+                    "error"-> "发生错误"
                     else -> return "&c-$node($lang)-".colored()
                 }
             }
@@ -30,6 +69,7 @@ private object Language{
                     "alias" -> "别名"
                     "permission" -> "許可權"
                     "parameter"-> "參數"
+                    "error"-> "發生錯誤"
                     else -> return "&c-$node($lang)-".colored()
                 }
             }
@@ -48,10 +88,12 @@ private var CommandComponent.description:String
 fun CommandComponent.description(value:String){
     description = value
 }
-
+private fun space(space: Int): String {
+    return (1..space).joinToString("") { " " }
+}
 
 fun CommandComponent.createHelper(check:Boolean = true,lang:String = "chinese"):CommandComponent{
-    fun name(cmd:CommandComponent):String{
+    fun name(cmd:CommandComponent,base:String):String{
         return when(cmd){
             is CommandComponentLiteral->{
                 cmd.aliases[0]
@@ -60,30 +102,28 @@ fun CommandComponent.createHelper(check:Boolean = true,lang:String = "chinese"):
                 cmd.comment
             }
             else ->{
-                "unknown"
+                base
             }
         }
     }
-    fun space(space: Int): String {
-        return (1..space).joinToString("") { " " }
-    }
-    fun preLink(cmd:CommandComponent):String{
-        var builder = ArrayList<String>()
-        if (cmd.parent != null){
-            builder.add(preLink(cmd.parent!!))
+    fun preLink(cmd:CommandComponent,base:String):String{
+        var result = ArrayList<String>()
+        if (cmd.parent == null){
+            result += name(cmd,base)
         }else{
-            builder.add(name(cmd))
+            result += name(cmd.parent!!,base)
+            result += preLink(cmd.parent!!,base)
         }
-        return builder.reversed().joinToString(" ")
+        return result.distinct().reversed().joinToString(" ")
     }
-    fun nextLink(cmd:CommandComponent):String{
+    fun nextLink(cmd:CommandComponent,base:String):String{
         if (cmd.children.isEmpty()){
-            return name(cmd)
+            return name(cmd,base)
         }
         var builder = ArrayList<String>()
-        builder.add(name(cmd))
+        builder.add(name(cmd,base))
         if (cmd.children.size == 1){
-            builder.add(nextLink(cmd.children[0]))
+            builder.add(nextLink(cmd.children[0],base))
         }else{
             builder.add("\\[...\\]")
         }
@@ -97,10 +137,10 @@ fun CommandComponent.createHelper(check:Boolean = true,lang:String = "chinese"):
         children.forEach {
             when(it){
                 is CommandComponentLiteral->{
-                    builder.append("    &7- [&7${nextLink(it)}](hover=${it.aliases.filter { index != 0 }.joinToString(",")};cmd=/${if (preLink(it) == "unknown") command.name+" "+it.aliases[0] else preLink(it)+" "+it.aliases[0]})\n".component().buildColored())
+                    builder.append("    &7- [&7${nextLink(it,command.name)}](hover=${it.aliases.filter { index != 0 }.joinToString(",")};cmd=/${preLink(it,command.name)+" "+it.aliases[0]})\n".component().buildColored())
                 }
                 is CommandComponentDynamic->{
-                    builder.append("    &7- [&7${nextLink(it)}](hover=${it.comment.replace("(","\\(").replace(")","\\)")};cmd=/${if (preLink(it) == "unknown") command.name else preLink(it)})\n".component().buildColored())
+                    builder.append("    &7- [&7${nextLink(it,command.name)}](hover=${it.comment.replace("(","\\(").replace(")","\\)")};suggest=/${preLink(it,command.name)})\n".component().buildColored())
                 }
             }
             builder.append("      &f[${it.description}](hover=${it.permission})\n".component().buildColored())
@@ -110,6 +150,19 @@ fun CommandComponent.createHelper(check:Boolean = true,lang:String = "chinese"):
     execute<ProxyCommandSender> { sender, context, argument ->
         val command = context.command
         val builder = "\n&7&l ${Language[lang,"command"]}: &c&l/${command.name} &f\\[ ${Language[lang,"alias"]}: &b&L${command.aliases.joinToString()}&f \\] &f\\[ ${Language[lang,"permission"]}: &b&L${command.permission}&f \\]\n".component().buildColored()
+        parent?.let {
+            when(this){
+                is CommandComponentLiteral->{
+                    builder.append(space(2)+"§c⇨ &7“${preLink(this,command.name)} ${this.aliases[0]}”...\n".colored())
+                }
+                is CommandComponentDynamic->{
+                    builder.append(space(2)+"§c⇨ &7“${preLink(this,command.name)} ${this.comment}”...\n".colored())
+                }
+                else -> {
+                    builder.append(space(2)+"§c⇨ &7“${preLink(this,command.name)}”...\n".colored())
+                }
+            }
+        }
         builder.append(space(4)+"&f&l${Language[lang,"parameter"]}:\n".colored())
         fun check(children: List<CommandComponent>): List<CommandComponent> {
             // 检查权限
@@ -131,19 +184,28 @@ fun CommandComponent.literalWithHelper(
     optional: Boolean = false,
     permission: String = "",
     hidden: Boolean = false,
-    checkPermission:Boolean = true,
-    lang:String = "zh_cn",
+    checkPermission: Boolean = true,
+    lang: String = "zh_cn",
     literal: CommandComponentLiteral.() -> Unit = {},
 ): CommandComponentLiteral {
-    return literal(aliases = aliases, optional = optional, permission = permission, hidden = hidden, literal = literal).createHelper(checkPermission,lang) as CommandComponentLiteral
+    return literal(
+        aliases = aliases,
+        optional = optional,
+        permission = permission,
+        hidden = hidden,
+        literal = literal
+    ).createHelper(checkPermission, lang) as CommandComponentLiteral
 }
 fun CommandComponent.dynamicWithHelper(
     comment: String = "...",
     optional: Boolean = false,
     permission: String = "",
-    checkPermission:Boolean = true,
-    lang:String = "zh_cn",
+    checkPermission: Boolean = true,
+    lang: String = "zh_cn",
     dynamic: CommandComponentDynamic.() -> Unit = {},
 ): CommandComponentDynamic {
-    return dynamic(comment, optional, permission, dynamic).createHelper(checkPermission,lang) as CommandComponentDynamic
+    return dynamic(comment, optional, permission, dynamic).createHelper(
+        checkPermission,
+        lang
+    ) as CommandComponentDynamic
 }
